907b62adf61e44cdfa5ebdf91ba80a2780bf5b06
GitiL <GitiL@skyvar.co.il>
2025-11-23 14:12:39 +0200

    After review

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import Sidebar, { SessionItem } from './components/sideBar/SideBar';
-
+import React from 'react';
 
 const MainApp: React.FC = () => {
-  const [sessions, setSessions] = useState<SessionItem[]>([]);
-
-
-  useEffect(() => {
-    const mock = [
-      {
-        id: "686af84bc0444adfb2257b390beee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },
-      {
-        id: "686af84bc0444adfb2257b390beee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },
-      {
-        id: "686af84bc0444adfb257b390beee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },
-      {
-        id: "686af84bc0444adfb2257b390beee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },
-      {
-        id: "686af84bc0444adfb2257b39eee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },
-      {
-        id: "686af84bc0444adfb2257b30beee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },  {
-        id: "686af84bc0444adfb2257b390bee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },
-      {
-        id: "686af84bc0444adfb2257b390beee9277",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },  {
-        id: "686af84bc0444adfb2257b390beee921",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },  {
-        id: "686af84bc0444adfb2257b390bee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },  {
-        id: "686af84bc0444adfb2257b390bee929",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      },  {
-        id: "686af84bc0444adfb2257b390beee92",
-        createdAt: "2025-11-23T06:56:01.935163Z"
-      }
-    ];
-
-
-    setSessions(mock);
-  }, []);
-
-
-  return <Sidebar sessions={sessions} />;
+    return <div>Chatbot-UI</div>;
 };
-
 
 export default MainApp;
