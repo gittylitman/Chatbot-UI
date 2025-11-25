0afec1d4594845859dc12144017842ba7c375c13
LeahMorgenshtern <LeaGol@skyvar.co.il>
2025-11-25 13:56:51 +0200

    :bulb: Removing ChatWindow import from App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,7 @@
-import { Box } from '@mui/material';
 import React from 'react';
 
-import ChatWindow from './components/mainPage/ChatWindow';
-
 const MainApp: React.FC = () => {
-    return (
-        <Box>
-            <ChatWindow />
-        </Box>
-    );
+    return <div>Chatbot-UI</div>;
 };
 
 export default MainApp;
